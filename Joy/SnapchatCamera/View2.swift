404da17e4a0dddbc2138a0a36764bc37a2c08dd6
Clara Carneiro <claracarneiro@Testess-MacBook-Pro.local>
2017-06-30 14:24:03 -0300

    Capture Picture, Switch Camera and Activate Flash

--- Joy/SnapchatCamera/View2.swift
+++ Joy/SnapchatCamera/View2.swift
@@ -9,21 +9,56 @@
 import UIKit
 import AVFoundation
 
-class View2: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+class View2: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet var cameraView: UIView!
     @IBOutlet var pickedImage: UIImageView!
-    
+
     var captureSession = AVCaptureSession()
-    var sessionOutput = AVCapturePhotoOutput()
-    var sessionOutputSetting = AVCapturePhotoSettings(format: [AVVideoCodecKey:AVVideoCodecJPEG])
-    var previewLayer = AVCaptureVideoPreviewLayer()
-    
+    var captureDevice: AVCaptureDevice?
+    var previewLayer : AVCaptureVideoPreviewLayer?
+    var stillImageOutput = AVCaptureStillImageOutput()
+    var frontCamera: Bool = false
+    var didtakePhoto = Bool()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-               
+        
+        captureSession.sessionPreset = AVCaptureSessionPresetPhoto
+        frontCamera(frontCamera)
+        if captureDevice != nil { beginSession() }
+        
         // Do any additional setup after loading the view.
+    }
+    
+    func beginSession() {
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        self.cameraView.layer.addSublayer(previewLayer!)
+        previewLayer?.frame = self.cameraView.layer.bounds
+        previewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
+        captureSession.startRunning()
+        stillImageOutput.outputSettings = [AVVideoCodecKey: AVVideoCodecJPEG]
+        if captureSession.canAddOutput(stillImageOutput) {captureSession.addOutput((stillImageOutput))}
+    }
+    
+    func frontCamera(_ front: Bool){
+        let devices = AVCaptureDevice.devices()
+        
+        do {try captureSession.removeInput(AVCaptureDeviceInput(device: captureDevice))} catch { print ("error while looking for devices")}
+        
+        for device in devices!{
+            if ((device as AnyObject).hasMediaType(AVMediaTypeVideo)){
+                if front {
+                    if (device as AnyObject).position == AVCaptureDevicePosition.front {
+                        captureDevice = device as? AVCaptureDevice
+                        do {try captureSession.addInput(AVCaptureDeviceInput(device:captureDevice))} catch { print ("error while setting camera position")}
+                        break }}
+                else {
+                    if (device as AnyObject).position == AVCaptureDevicePosition.back {
+                        captureDevice = device as? AVCaptureDevice
+                            
+                        do {try captureSession.addInput(AVCaptureDeviceInput(device:captureDevice))} catch { print ("error while setting camera position")}
+                        break }}}}
     }
     
     override func didReceiveMemoryWarning() {
@@ -31,63 +66,53 @@ class View2: UIViewController, UIImagePickerControllerDelegate, UINavigationCont
         // Dispose of any resources that can be recreated.
     }
     
-    override func viewDidAppear(_ animated: Bool){
-        super.viewDidAppear(animated)
-        previewLayer.frame = cameraView.bounds
-        
+    
+    @IBAction func activateFlash(_ sender: UIButton) {
+        if captureDevice!.hasTorch{
+            do {try captureDevice!.lockForConfiguration()
+                captureDevice!.torchMode = captureDevice!.isTorchActive ? AVCaptureTorchMode.off : AVCaptureTorchMode.on
+                captureDevice!.unlockForConfiguration()
+            } catch { print ("error while trying to activate flash") }
+        }
     }
     
-    override func viewWillAppear (_ animated: Bool) {
-        super.viewWillAppear(animated)
-
-        let deviceDiscoverySession = AVCaptureDeviceDiscoverySession(deviceTypes: [AVCaptureDeviceType.builtInDuoCamera, AVCaptureDeviceType.builtInTelephotoCamera,AVCaptureDeviceType.builtInWideAngleCamera], mediaType: AVMediaTypeVideo, position: AVCaptureDevicePosition.unspecified)
+    @IBAction func setCamera(_ sender: UIButton) {
+        //switch camera to frontal 
+        frontCamera = !frontCamera
+        captureSession.beginConfiguration()
+        let inputs = captureSession.inputs as! [AVCaptureInput]
+        for oldInput: AVCaptureInput in inputs {captureSession.removeInput(oldInput)}
         
-        for device in (deviceDiscoverySession?.devices)! {
-            if(device.position == AVCaptureDevicePosition.front){
-                do {
-                    let input = try AVCaptureDeviceInput(device: device)
-                    
-                    if(captureSession.canAddInput(input)){
-                        captureSession.addInput(input)
-                        
-                        if(captureSession.canAddOutput(sessionOutput)){
-                            
-                            captureSession.addOutput(sessionOutput)
-                            previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-                            previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
-                            previewLayer.connection.videoOrientation = AVCaptureVideoOrientation.portrait
-                            cameraView.layer.addSublayer(previewLayer)
-                            
-                            captureSession.startRunning()}}
-                } catch{ print("exception on front camera!") }}}
+        frontCamera(frontCamera)
+        captureSession.commitConfiguration()
     }
     
     
-//    @IBAction func cameraButtonAction(_ sender: UIButton) {
-//        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera){
-//            let imagePicker = UIImagePickerController()
-//            imagePicker.sourceType = UIImagePickerControllerSourceType.camera
-//            imagePicker.allowsEditing = false
-//            self.present(imagePicker, animated: true, completion: nil)}
-//    }
-//    
-//    @IBAction func saveAction(_ sender: UIButton) {
-//        let imageData = UIImageJPEGRepresentation(pickedImage.image!, 0.6)
-//        let compressedJPEGImage = UIImage(data: imageData!)
-//        UIImageWriteToSavedPhotosAlbum(compressedJPEGImage!, nil, nil, nil)
-//        saveNotice()
-//    }
-//    
-//    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info : [String: Any]){
-//        
-//        pickedImage.image = info[UIImagePickerControllerOriginalImage] as! UIImage!
-//        self.dismiss(animated: true, completion: nil)
-//    }
-//    
-//    func saveNotice(){
-//        let alertController = UIAlertController(title: "Imagem Salva", message: "Sua foto foi salva com sucesso", preferredStyle: .alert)
-//        let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
-//        alertController.addAction(defaultAction)
-//        present(alertController, animated: true, completion: nil)
-//    }
+    @IBAction func startCapture(_ sender: UIButton) {
+        captureAnotherPicture()
+    }
+    
+    func capturePicture() {
+        if let videoConnection = stillImageOutput.connection(withMediaType: AVMediaTypeVideo){
+            stillImageOutput.captureStillImageAsynchronously(from: videoConnection, completionHandler: { (imageDataSampleBuffer, error) in
+                    let imageData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(imageDataSampleBuffer)
+                    let image = UIImage(data: imageData!)
+                    print("image taked: \(image)")
+                    self.pickedImage.image = image!
+                    self.cameraView.addSubview(self.pickedImage)
+                    self.pickedImage.isHidden = false})
+        }
+    }
+    
+    func captureAnotherPicture(){
+        if didtakePhoto == true {
+            self.pickedImage.isHidden = true
+            didtakePhoto = false}
+        else {
+            captureSession.startRunning()
+            didtakePhoto = true
+            capturePicture()}
+        
+    }
+    
 }
